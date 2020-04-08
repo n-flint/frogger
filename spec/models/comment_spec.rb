0d30ca5299e1464a45b49434d9f2a88aa1d50ca3
Noah Flint <noah.flint24@gmail.com>
2020-04-08 14:15:11 -0600

    Add comment model spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 describe Comment, type: :model do
-  describe 'relationships' do
-    it {should belong_to (:article)}
-  end
-
   describe 'validations' do
     it {should validate_presence_of(:body)}
+  end
+
+  describe 'relationships' do
+    it {should belong_to (:article)}
   end
 end
