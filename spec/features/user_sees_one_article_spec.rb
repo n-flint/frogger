f6f57c8ebbd2a3fbee9f8f9fe7e6e3f0d131013b
Noah Flint <noah.flint24@gmail.com>
2020-04-08 14:44:10 -0600

    Add comment form test to article show spec

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -25,4 +25,21 @@ describe 'user sees one article' do
       expect(page).to_not have_content(article_2.title)
     end
   end
+
+  describe "they fill in a comment form" do
+    it "displays the comment on the article show" do
+      article = Article.create!(title: "New Title", body: "New Body")
+
+      visit article_path(article)
+
+      fill_in "comment[author_name]", with: "Doc Antel"
+      fill_in "comment[body]", with: "I would never euthenize my tigers, that's crazy..."
+      click_on "Submit"
+
+      expect(current_path).to eq(article_path(article))
+      expect(page).to have_content("Post a Comment")
+      expect(page).to have_content("Doc Antel")
+      expect(page).to have_content("I would never euthenize my tigers, that's crazy...")
+    end
+  end
 end
