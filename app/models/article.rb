047bc57f789de19beaca79384ac517b03bc94abb
Noah Flint <noah.flint24@gmail.com>
2020-04-10 18:25:47 -0600

    Add relationships to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
 
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
   validates_presence_of :title,
                         :body
 
