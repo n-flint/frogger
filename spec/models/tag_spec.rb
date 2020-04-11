46e32842f06ed322bd1bf3cb646871713b499b0d
Noah Flint <noah.flint24@gmail.com>
2020-04-10 19:32:29 -0600

    Add test for #tags_list

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -4,4 +4,15 @@ describe Tag, type: :model do
   describe 'realationships' do
     it {should have_many :taggings}
   end
+
+  describe 'instance methods' do
+    it '#tags_list' do
+      article = Article.create!(title: 'Article Uno', body: 'This is the first article')
+      article.tags.create(name: 'Tag 1')
+      article.tags.create(name: 'Tag 2')
+      
+      expect(article.tags.first.name).to eq('Tag 1')
+      expect(article.tags.second.name).to eq('Tag 2')
+    end
+  end
 end
