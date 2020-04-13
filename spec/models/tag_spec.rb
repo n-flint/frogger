b7b19cad314a14740eeaa191e8e6b1ee37d67905
Noah Flint <noah.flint24@gmail.com>
2020-04-13 11:18:46 -0600

    Add model spec for tag

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -10,9 +10,8 @@ describe Tag, type: :model do
       article = Article.create!(title: 'Article Uno', body: 'This is the first article')
       article.tags.create(name: 'Tag 1')
       article.tags.create(name: 'Tag 2')
-      
-      expect(article.tags.first.name).to eq('Tag 1')
-      expect(article.tags.second.name).to eq('Tag 2')
+
+      expect(article.tag_list).to eq('Tag 1, Tag 2')
     end
   end
 end
