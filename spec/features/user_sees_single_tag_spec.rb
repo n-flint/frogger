644b64c8a34e5ae4f0340d9ab6cf5334ffe159d6
Noah Flint <noah.flint24@gmail.com>
2020-04-13 14:15:50 -0600

    Add tag show spec

---
+++ spec/features/user_sees_single_tag_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe 'user sees sinlge tag' do
+  describe 'they link from article show page' do
+    it 'shows all tag information' do
+      article = Article.create(title: 'Title Uno', body: 'This is the body for article numero uno')
+
+      tag = Tag.create(name: 'Interesting')
+      article.tags << tag
+
+      visit article_path(article)
+
+      click_link 'tag 1'
+
+      expect(current_path).to eq(tag_path(tag))
+      expect(page).to have_content('tag 1')
+    end
+  end
+end
