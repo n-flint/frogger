aa644a4ffdb6c6bccbfbc5a7b60b443190f58499
Noah Flint <noah.flint24@gmail.com>
2020-04-20 16:05:23 -0600

    Add destroy action and tag_params strong params

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,6 +5,19 @@ class TagsController < ApplicationController
   end
 
   def show
-    @tag = Tag.find(params[:id])
+    @tag = Tag.find(tag_params[:id])
   end
+
+  def destroy
+    tag = Tag.find(tag_params[:id])
+    flash.notice = "Tag '#{tag.name}' was Deleted!"
+
+    tag.destroy
+    redirect_to tags_path
+  end
+
+  private
+    def tag_params
+      params.permit(:id)
+    end
 end
