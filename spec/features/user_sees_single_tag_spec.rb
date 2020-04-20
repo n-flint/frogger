f81e666f557211433ed1e28fbd35bca9d73ad225
Noah Flint <noah.flint24@gmail.com>
2020-04-20 12:58:18 -0600

    Add link from tags index page to tag show page

--- spec/features/user_sees_single_tag_spec.rb
+++ spec/features/user_sees_single_tag_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 describe 'user sees sinlge tag' do
-  describe 'they link from article show page' do
+  describe 'they link from tags index page' do
     it 'shows all tag information' do
       article = Article.create(title: 'Title Uno', body: 'This is the body for article numero uno')
 
       tag = Tag.create(name: 'Interesting')
       article.tags << tag
 
-      visit article_path(article)
+      visit tags_path
 
       click_link 'Interesting'
 
